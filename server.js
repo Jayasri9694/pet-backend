2e5e7b3ca920e8219c9dc71e24dacf4b1f444462
jaya <kjayashree061@gmail.com>
2024-12-12 21:07:48 +0530

    commit

--- server.js
+++ server.js
@@ -17,7 +17,9 @@ app.get('/', (_req, res) => {
   });
   
 app.use(cors({
-  origin: ['http://localhost:5173']
+  origin: 'http://localhost:5173',
+    methods: ['GET', 'POST'],
+    credentials: true
 }));
 
 // Use the user routes
